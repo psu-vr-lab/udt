aa259f1a3476f9094d57494133b104bbc10ed3ff
jejikeh <jejikeh@gmail.com>
2022-07-07 00:15:46 +0300

    Add project files.

---
+++ UnrealSetupper/Program.cs
@@ -0,0 +1,64 @@
+﻿using UnrealSetupper;
+using System.Text.Json;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        string userArgs = string.Empty;
+        if(args.Length > 0)
+        {
+            foreach(string arg in args)
+            {
+                userArgs += arg;
+            }
+        }
+
+        if (!File.Exists("UnrealSettuper.config.json"))
+        {
+            ConfigOutput();
+        }
+
+        if (userArgs == "config")
+        {
+            ConfigOutput();
+        }
+        else
+        {
+            string configName = "UnrealSettuper.config.json";
+            USettuperConfig? config = JsonSerializer.Deserialize<USettuperConfig>(File.ReadAllText(configName));
+            if(config == null || config.ProjectsDir == null || config.UnrealDir == null)
+            {
+                return;
+            }
+            string projectDir = Path.Combine(config.ProjectsDir, userArgs);
+            string sourceDir = Path.Combine(projectDir, "Source");
+            string coreDir = Path.Combine(sourceDir, $"{userArgs}Core");
+            string privateDir = Path.Combine(coreDir,"Private");
+            string publicDir = Path.Combine(coreDir, "Public");
+
+            Directory.CreateDirectory(projectDir);
+            Directory.CreateDirectory(sourceDir);
+            Directory.CreateDirectory(coreDir);
+            Directory.CreateDirectory(privateDir);
+            Directory.CreateDirectory(publicDir);
+
+
+            USettuper.UUPROJ(Path.Combine(projectDir, userArgs) + ".uproject",userArgs);
+            USettuper.UMT(Path.Combine(sourceDir,userArgs) + ".Target.cs", userArgs,"Game");
+            USettuper.UMT(Path.Combine(sourceDir, userArgs) + "Editor.Target.cs", userArgs, "Editor");
+            USettuper.UMB(Path.Combine(coreDir, userArgs) + "Core.Build.cs", userArgs);
+            USettuper.UMH(Path.Combine(publicDir, userArgs) + "Core.h", userArgs);
+        }
+    }
+
+    private static void ConfigOutput()
+    {
+        Console.Write("Unreal path: ");
+        string? unrealDir = Console.ReadLine();
+        Console.Write("Projects directory path: ");
+        string? projectsDir = Console.ReadLine();
+
+        USettuper.Config(unrealDir, projectsDir);
+    }
+}
---
+++ UnrealSetupper/USettuper.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.Json;
+namespace UnrealSetupper
+{
+    internal class USettuperConfig
+    {
+        public string? UnrealDir { get; set; }
+        public string? ProjectsDir { get; set; }
+    }
+
+    internal static class USettuper
+    {
+        public static void Config(string? unrealDir, string? projectsDir)
+        {
+            USettuperConfig config = new USettuperConfig
+            {
+                UnrealDir = unrealDir,
+                ProjectsDir = projectsDir
+            };
+            string fileName = "UnrealSettuper.config.json";
+            string configToJson = JsonSerializer.Serialize(config);
+            File.WriteAllText(fileName, configToJson);
+
+            //Console.Write(File.ReadAllText(fileName));
+        }
+        /// <summary>
+        /// Project .uproject file
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="projectName"></param>
+        public static void UUPROJ(string filePath,string projectName)
+        {
+            try
+            {
+                using (StreamWriter sw = File.CreateText(Path.Combine(filePath)))
+                {
+                    sw.WriteLine(@"{
+    ""FileVersion"": 3,
+    ""EngineAssociation"": ""4.27"",
+    ""Category"": """",
+    ""Description"": """",
+    ""Modules"": [
+        {
+            ""Name"":" + $"\"{projectName}Core\"" + "," + @"
+            ""Type"": ""Runtime"",
+            ""LoadingPhase"": ""Default""
+        }
+    ]
+}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+        /// <summary>
+        /// Unreal Module Target
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="projectName"></param>
+        /// <param name="targetType"></param>
+        public static void UMT(string filePath, string projectName, string targetType)
+        {
+            string className = string.Empty;
+            if(targetType == "Editor")
+            {
+                className = targetType;
+            }
+            try
+            {
+                using (StreamWriter sw = File.CreateText(Path.Combine(filePath)))
+                {
+                    sw.WriteLine(@"using UnrealBuildTool;
+public class " + projectName + className + @"Target : TargetRules
+{
+    public " + projectName + className + @"Target(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType." + targetType + @";
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+        ExtraModuleNames.AddRange(new string[] { " + $"\"{projectName}Core\"" + @" });
+    }
+}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+        /// <summary>
+        /// Unreal Module Build
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="projectName"></param>
+        public static void UMB(string filePath, string projectName)
+        {
+            try
+            {
+                using (StreamWriter sw = File.CreateText(Path.Combine(filePath)))
+                {
+                    sw.WriteLine(@"using UnrealBuildTool;
+public class " + $"{projectName}" + @"Core : ModuleRules
+{
+	public BobCore(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		bEnforceIWYU = true;
+		PublicDependencyModuleNames.AddRange(new string[] {" + $" \"Core\", \"CoreUObject\", \"Engine\" " + @"});
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+
+    }
+}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+        /// <summary>
+        /// Unreal Module Header
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="projectName"></param>
+        public static void UMH(string filePath, string projectName)
+        {
+            try
+            {
+                using (StreamWriter sw = File.CreateText(Path.Combine(filePath)))
+                {
+                    sw.WriteLine(@"#pragma once
+#include ""CoreMinimal.h""
+#include ""Modules/ModuleInterface.h""
+class F" + $"{projectName}" + @"Core : public IModuleInterface
+{
+public:
+	static inline F" +$"{projectName}" +@"Core& Get()
+        {
+            return FModuleManager::LoadModuleChecked<F"+$"{projectName}"+ @"Core>(" + $"{projectName}" + @");
+        }
+
+        static inline bool IsAvailable()
+        {
+            return FModuleManager::Get().IsModuleLoaded(" + $"{projectName}" + @");
+        }
+
+        virtual void StartupModule() override;
+	    virtual void ShutdownModule() override;
+};");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+    }
+}
