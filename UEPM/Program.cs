0d8088c2647469dfcf1013d621a0fcc7d5cc1481
jejikeh <jejikeh@gmail.com>
2022-07-10 14:22:00 +0300

    link unreal projects, run compiled exe file

--- UEPM/Program.cs
+++ UEPM/Program.cs
@@ -61,6 +61,26 @@ class Program
             Output.Error("There is no such project");
 
         }
+        else if (userArgs.StartsWith("run", comparison))
+        {
+            string projectName = userArgs.Replace("run", "");
+
+            DirectoryInfo projectsDir = new DirectoryInfo(appDir + @"Projects");
+            FileInfo[] files = projectsDir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                if (projectName == file.Name.Replace($".config.json", ""))
+                {
+                    USettuperProjectConfig? projectConfig = JsonSerializer.Deserialize<USettuperProjectConfig>(File.ReadAllText(appDir + @"Projects\" + $"{projectName}.config.json"));
+                    if (projectConfig != null && projectConfig.ProjectDir != null)
+                        System.Diagnostics.Process.Start($" {projectConfig.ProjectDir}" + @"\Binaries\Win64\" + $"{projectConfig.Name}.exe");
+                    Output.Succses("OK!");
+                    return;
+                }
+            }
+            Output.Error("There is no such project");
+
+        }
         else if (userArgs.StartsWith("Build", comparison))
         {
             BatRunOutput(userArgs, "Build");
@@ -76,6 +96,63 @@ class Program
         else if (userArgs.StartsWith("Cook", comparison))
         {
             BatRunOutput(userArgs, "Cook");
+        }
+        else if (userArgs.StartsWith("Link", comparison))
+        {
+            USettuperConfig? config = JsonSerializer.Deserialize<USettuperConfig>(File.ReadAllText(appDir + "UnrealSettuper.config.json"));
+            if (config == null || config.ProjectsDir == null || config.UnrealDir == null)
+            {
+                Output.Error("Config error!");
+                return;
+            }
+
+            string projectPath = userArgs.Replace("link", "");
+            DirectoryInfo projectsDir = new DirectoryInfo(projectPath);
+            FileInfo[] files = projectsDir.GetFiles();
+            bool wasFoundProjectBat = false;
+            bool wasFoundUnrealProjectFile = false;
+
+            string unrealProjectName = string.Empty;
+            foreach (FileInfo file in files)
+            {
+                if ("Build.bat" == file.Name)
+                {
+                    wasFoundProjectBat = true;
+                }
+
+                if (file.Name.Contains(".uproject"))
+                {
+                    wasFoundUnrealProjectFile = true;
+                    unrealProjectName = Path.GetFileNameWithoutExtension(projectPath + @"\" + $"{file.Name}");
+                    Console.WriteLine(unrealProjectName);
+                }
+            }
+            if (!wasFoundUnrealProjectFile)
+            {
+                Output.Error("No unreal files was found.");
+                return;
+            }
+
+            USettuperProjectConfig projectConfig = new USettuperProjectConfig
+            {
+                Name = unrealProjectName,
+                ProjectDir = projectPath
+            };
+            if (!wasFoundProjectBat)
+            {
+                Console.WriteLine("Start generating uepme build files");
+                USettuper.GBB(Path.Combine(projectPath, "Build") + ".bat", unrealProjectName, config);
+                USettuper.GCB(Path.Combine(projectPath, "Compile") + ".bat", unrealProjectName, config);
+                USettuper.GCoB(Path.Combine(projectPath, "Cook") + ".bat", unrealProjectName, config);
+                USettuper.GEB(Path.Combine(projectPath, "Editor") + ".bat", unrealProjectName, config);
+            }
+
+            string fileName = appDir + @"Projects\" + $"{unrealProjectName}.config.json";
+            string configToJson = JsonSerializer.Serialize(projectConfig);
+            File.WriteAllText(fileName, configToJson);
+            Output.Succses("Ok!");
+
+            
         }
         else if (userArgs.StartsWith("List", comparison))
         {
@@ -169,7 +246,7 @@ class Program
 
         if (Directory.Exists(projectDir))
         {
-            Output.Error("Folder with that name already created!");
+            Output.Error("Folder with that name already created, Use Link instead.");
             return;
         }
 
