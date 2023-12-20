8b5f82b835498c95d7a3d2729259d5cc5efbc7b6
jejikeh <jejikeh@gmail.com>
2023-12-20 19:07:05 +0300

    refactor: change property in UnrealEngineAssociation to init only, add overrided GetHashCode() methods for HashSet usage

--- Ueco.CLI/Models/UnrealEngineAssociation.cs
+++ Ueco.CLI/Models/UnrealEngineAssociation.cs
@@ -10,21 +10,33 @@ public class UnrealEngineAssociation
     /// <summary>
     /// Name of the Engine. For example, "ueco-engine"
     /// </summary>
-    public required string Name { get; set; }
+    public required string Name { get; init; }
     
     /// <summary>
     /// Path to the Engine folder. For example, "C:\Program Files\Unreal Engine\4.27\"
     /// The folder should contain "Engine"
     /// </summary>
-    public required string Path { get; set; }
+    public required string Path { get; init; }
     
     /// <summary>
     /// Version of the Engine
     /// </summary>
-    public UnrealEngineVersion Version { get; set; }
+    public UnrealEngineVersion Version { get; init; }
     
     /// <summary>
     /// Use this Engine by default. If true, it will be used by default when no other Engine is selected
     /// </summary>
     public bool IsDefault { get; set; }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null) return false;
+        
+        return Name == (obj as UnrealEngineAssociation)?.Name;
+    }
 }
